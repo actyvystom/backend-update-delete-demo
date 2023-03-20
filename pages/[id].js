06d1fb96d5d1aa874dd8f792465814e6f4439c03
actyvystom <thomas.goerldt@actyvyst.com>
2023-03-20 16:02:17 +0100

    add demo end

--- pages/[id].js
+++ pages/[id].js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import Joke from "../components/Joke";
+import useSWRMutation from "swr/mutation";
 
 export default function JokeDetailsPage() {
   const router = useRouter();
@@ -8,9 +9,43 @@ export default function JokeDetailsPage() {
     push,
   } = router;
 
-  async function handleEditJoke() {}
+  const { trigger, isMutating } = useSWRMutation(
+    `/api/jokes/${id}`,
+    sendRequest
+  );
+  async function sendRequest(url, { arg }) {
+    const response = await fetch(url, {
+      method: "PUT",
+      body: JSON.stringify(arg),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      await response.json();
+    } else {
+      console.error(`Error: ${response.status}`);
+    }
+  }
 
-  async function handleDeleteJoke() {}
+  async function handleEditJoke(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
 
+    const jokeData = Object.fromEntries(formData);
+    console.log(jokeData);
+    await trigger(jokeData);
+    push("/");
+  }
+
+  async function handleDeleteJoke() {
+    await fetch(`/api/jokes/${id}`, {
+      method: "DELETE",
+    });
+    push("/");
+  }
+  if (isMutating) {
+    return <h1>Submitting your changes...</h1>;
+  }
   return <Joke onSubmit={handleEditJoke} onDelete={handleDeleteJoke} />;
 }
--- pages/api/jokes/[id].js
+++ pages/api/jokes/[id].js
@@ -5,6 +5,17 @@ export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
+  if (request.method === "PUT") {
+    const jokeToUpdate = await Joke.findByIdAndUpdate(id, {
+      $set: request.body,
+    });
+    response.status(200).json(jokeToUpdate);
+  }
+  if (request.method === "DELETE") {
+    const jokeToDelete = await Joke.findByIdAndDelete(id);
+    response.status(200).json(jokeToDelete);
+  }
+
   if (request.method === "GET") {
     const joke = await Joke.findById(id);
 
